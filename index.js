f7d8b1ee8e2febdcfb5ad106b2596e5b1f9e951e
Andrey Sery <andrey.72006@gmail.com>
2024-06-27 11:17:38 +0300

    deleted project

--- index.js
+++
@@ -1,3 +0,0 @@
-import { App } from "./src/ui/app.js";
-
-App()
--- src/ui/Button.js
+++
@@ -1,9 +0,0 @@
-export function Button(method, name) {
-  const buttonElement = document.createElement("button")
-  buttonElement.textContent = name
-  buttonElement.addEventListener("click", ()=> {
-    method()
-  })
-
-  return buttonElement
-}
